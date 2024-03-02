cbc7c7bb7c3594a9b1dd54b304f376f73a6de911
Christian Hammond <christian@beanbaginc.com>
2024-03-02 15:47:04 -0800

    Add a Babel preset/plugin for compiling craft and paint templates.
    This new preset can be used in consuming applications to compile any
    `craft` or `paint` tagged template literals into their raw structured
    form, reducing the runtime costs of an application. It's recommended
    whenever consuming Ink in another application.
    
    Testing Done:
    Tested this in Review Board.
    
    Reviewed at https://reviews.reviewboard.org/r/13590/

---
+++ src/babel-preset/index.js
@@ -0,0 +1,46 @@
+/**
+ * Babel plugin that compiles ``paint`` and ``craft`` tagged template literals.
+ *
+ * This Babel plugin will convert any painting or crafting template literals
+ * into their compiled forms, reducing the runtime of the application. It's
+ * recommended for any projects using Ink.
+ *
+ * Version Added:
+ *     1.0
+ */
+export default function inkPreset(api, options={}) {
+    api.cache(true);
+
+    return {
+        plugins: [
+            [
+                'htm',
+                {
+                    'import': {
+                        'export': 'craftComponent',
+                        'module': '@beanbag/ink',
+                    },
+                    'pragma': 'craftComponent',
+                    'tag': 'craft',
+                    'useBuiltins': true,
+                    'useNativeSpread': true,
+                },
+                'ink-htm-craft',
+            ],
+            [
+                'htm',
+                {
+                    'import': {
+                        'export': 'paintComponent',
+                        'module': '@beanbag/ink',
+                    },
+                    'pragma': 'paintComponent',
+                    'tag': 'paint',
+                    'useBuiltins': true,
+                    'useNativeSpread': true,
+                },
+                'ink-htm-paint',
+            ],
+        ],
+    };
+}
